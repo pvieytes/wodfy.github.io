f5ddc8ffd0359264aca36f477b6c918370bd5bcf
Pablo Vieytes <pablo@onytes.com>
2016-04-04 16:17:59 +0200

    add select instrument

--- js/app/ony-ear.js
+++ js/app/ony-ear.js
@@ -1,8 +1,14 @@
 define( ["jquery"], function ($) {
 
+	var instruments,
+		offset;
+
+
+
     function init(){
         console.log('init ony-ear');
-        var delay = 0; // play one note every quarter second
+        initInstrument();
+        /*var delay = 0; // play one note every quarter second
         var note = 50; // the MIDI note
         var velocity = 127; // how hard the note hits
         
@@ -10,10 +16,28 @@ define( ["jquery"], function ($) {
         MIDI.setVolume(0, 127);
         MIDI.noteOn(0, note, velocity, delay);
         MIDI.noteOff(0, note, delay +0.75);
-
-
+		*/
     }
 
+    function initInstrument(){
+    	// init offset
+    	instruments = {
+    		'C':0,
+    		'Bb': -2,
+    		'Eb': 3
+    	}
+
+		$instrument = $('.js-instrument-select');
+		for (k in instruments){
+			$instrument.append('<option>' + k + '</option>');
+		}
+		offset = 0;
+		$('.js-instrument-select').change(function(){
+			var sel;
+			sel = $(this).find(":selected").text();
+			offset = instruments[sel];
+		});
+    }
 
     return {
         init:init,

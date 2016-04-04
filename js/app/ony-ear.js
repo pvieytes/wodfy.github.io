5b9fb78d19cdf4de599bcfd369fc723ebd285a65
Pablo Vieytes <pablo@onytes.com>
2016-04-04 16:00:03 +0200

    add to require

--- js/app/ony-ear.js
+++ js/app/ony-ear.js
@@ -2,28 +2,16 @@ define( ["jquery"], function ($) {
 
     function init(){
         console.log('init ony-ear');
+        var delay = 0; // play one note every quarter second
+        var note = 50; // the MIDI note
+        var velocity = 127; // how hard the note hits
+        
+        // play the note
+        MIDI.setVolume(0, 127);
+        MIDI.noteOn(0, note, velocity, delay);
+        MIDI.noteOff(0, note, delay +0.75);
 
 
-
-        window.onload = function () {
-			MIDI.loadPlugin({
-				soundfontUrl: "./soundfont/",
-				instrument: "acoustic_grand_piano",
-				onprogress: function(state, progress) {
-					console.log(state, progress);
-				},
-				onsuccess: function() {
-					var delay = 0; // play one note every quarter second
-					var note = 50; // the MIDI note
-					var velocity = 127; // how hard the note hits
-					// play the note
-					MIDI.setVolume(0, 127);
-					MIDI.noteOn(0, note, velocity, delay);
-					MIDI.noteOff(0, note, delay + 0.75);
-				}
-			});
-		};
-		
     }
 
 
